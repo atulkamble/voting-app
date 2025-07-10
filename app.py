a5c6469773f2db0d7fc7f093dbc7847b4c358516
Atul Kamble <atul_kamble@hotmail.com>
2025-07-10 15:03:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
@@ -14,6 +14,16 @@ def vote():
     if language in votes:
         votes[language] += 1
     return redirect(url_for('index'))
+
+@app.route('/reset', methods=['POST'])
+def reset_votes():
+    for key in votes:
+        votes[key] = 0
+    return redirect(url_for('index'))
+
+@app.route('/data')
+def data():
+    return jsonify(votes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
